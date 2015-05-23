9f20ccd238f1ce733d06c36366cb2546007abeab
Iglesias <Iglesias@gmail.com>
2015-05-23 12:27:30 +0200

    Volvemos a cambiar el atributo fecha_hora de la clase Venta a tipo Date

--- Tienda_Hector_010/src/tienda/Venta.java
+++ Tienda_Hector_010/src/tienda/Venta.java
@@ -1,12 +1,12 @@
 package tienda;
-
+import java.util.Date;
 public class Venta {
 	
 	String id_Cliente;
 	String id_Producto;
 	String id_Vendedor;
 	String id_tienda;
-	String fecha_hora;
+	Date fecha_hora;
 	public String getId_Cliente() {
 		return id_Cliente;
 	}
@@ -31,10 +31,10 @@ public class Venta {
 	public void setId_tienda(String id_tienda) {
 		this.id_tienda = id_tienda;
 	}
-	public String getFecha_hora() {
+	public Date getFecha_hora() {
 		return fecha_hora;
 	}
-	public void setFecha_hora(String fecha_hora) {
+	public void setFecha_hora(Date fecha_hora) {
 		this.fecha_hora = fecha_hora;
 	}
 	
