0ee698b42283eadac326629ea4165667e1cdeda9
Iglesias <Iglesias@gmail.com>
2015-05-23 12:21:31 +0200

    Añadimos un atributo a Vendedores cins get y set

--- Tienda_Hector_010/src/tienda/Vendedores.java
+++ Tienda_Hector_010/src/tienda/Vendedores.java
@@ -1,5 +1,17 @@
 package tienda;
 
 public class Vendedores {
+	
+	String id_Vendedor;
+
+	public String getId_Vendedor() {
+		return id_Vendedor;
+	}
+
+	public void setId_Vendedor(String id_Vendedor) {
+		this.id_Vendedor = id_Vendedor;
+	}
+	
+	
 
 }
